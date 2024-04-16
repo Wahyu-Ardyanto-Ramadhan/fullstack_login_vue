993124f21db81b157f4618d6b67eabcfaba2cf65
wahyu ardyanto <ardyantowahyu11@gmail.com>
2024-04-16 23:36:39 +0700

    maintenance server BE

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { envs, Client } from 'stytch';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 import cors from'cors';
 
-dotenv.config();
+dotenv.config()
 
 const app = express();
 
